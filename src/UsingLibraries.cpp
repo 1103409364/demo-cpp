0a2373e0603bc4d5a889e1a62692cf2a07d45bc7
wcs <wcs78929@gmail.com>
2022-10-30 09:53:45 +0800

    去除 GLFW
    Signed-off-by: wcs <wcs78929@gmail.com>

--- src/UsingLibraries.cpp
+++ src/UsingLibraries.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <GLFW/glfw3.h>
+// #include <GLFW/glfw3.h>
 // extern "C" int glfwInit(); // extern "C" 链接到 c 库
-void testUsingLibraries()
-{
-  int a = glfwInit();
-  std::cout << "GLFW:" << a << std::endl;
+void testUsingLibraries(){
+    // int a = glfwInit();
+    // std::cout << "GLFW:" << a << std::endl;
 };
